c7810f3f2f55764b67e2d5c63bfd1b5830949a13
Razvan789 <razvanbeldeanu789@gmail.com>
2022-03-03 19:26:30 -0500

    Reformated parts of the project to use PHP files instead of HTML

--- src/canvas.php
+++ src/canvas.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -6,16 +8,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GrafStateTest</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-        <script src="/src/scripts/canvas.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="/scripts/canvas.js" defer></script>
 </head>
 
 <body>
@@ -24,9 +21,7 @@
             <a class="navbar-brand" href="#">
                 <img src="/assets/Logo.png" height="65" alt="Gra𝑓sτaτe">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -62,8 +57,7 @@
         New drawing
     </button>
     <!-- Modal -->
-    <div class="modal fade" id="canvasModal" tabindex="-1" role="dialog" aria-labelledby="canvasModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="canvasModal" tabindex="-1" role="dialog" aria-labelledby="canvasModalLabel" aria-hidden="true">
         <div id="canvas-modal-dialog" class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -76,8 +70,7 @@
                     <div class="canvasControls">
                         <button id="clearCanv" class="btn btn-outline-secondary" type="button">Clear</button>
                         <label>Color:</label>
-                        <input type="color" class="form-control form-control-color" id="color-picker" value="#563d7c"
-                            title="Choose your color">
+                        <input type="color" class="form-control form-control-color" id="color-picker" value="#563d7c" title="Choose your color">
                         <label for="size-picker">Size:</label>
                         <select name="size-picker" id="size-picker">
                             <option value="1" selected>1px</option>
@@ -92,8 +85,7 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button id="saveCanv" type="button" class="btn btn-primary" data-bs-dismiss="modal">Save</button>
-                    <button type="button" id="update-canvas-btn" class="d-none btn btn-primary"
-                        data-bs-dismiss="modal">Update</button>
+                    <button type="button" id="update-canvas-btn" class="d-none btn btn-primary" data-bs-dismiss="modal">Update</button>
                 </div>
             </div>
         </div>
@@ -108,12 +100,12 @@
 
     <template id="card-template">
         <button class="card" data-bs-toggle="modal" data-bs-target="#canvasModal">
-            <img class="card-img-top" id ="canImg" width="200" height="200">
+            <img class="card-img-top" id="canImg" width="200" height="200">
             <div class="card-body">
                 <h5 class="card-title">Saved Image</h5>
             </div>
         </button>
-    </template>   
+    </template>
 </body>
 
 </html>
