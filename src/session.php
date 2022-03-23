0ce9171bf37c9ef0f568ea40cebdb7d0c4f4b95b
Razvan789 <razvanbeldeanu789@gmail.com>
2022-03-23 19:18:41 -0400

    Converted to PDO connection

--- src/session.php
+++ src/session.php
@@ -3,10 +3,11 @@
    session_start();
    
    $user_check = $_SESSION['login_user'];
-   
-   $ses_sql = mysqli_query($db,"select FirstName from logintable where Email = '$user_check' ");
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   $sql = "select FirstName from logintable where Email = '$user_check' ";
+   $statement = $db->prepare($sql);
+   $statement->execute();
+   $row = $statement->fetch();
+   $statement->closeCursor();
    
    $login_session_name = $row['FirstName'];
    
