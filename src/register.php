1555b7a36c38f07f96d83bb04abfc49ce8082d7b
Razvan789 <razvanbeldeanu789@gmail.com>
2022-03-23 20:33:52 -0400

    Converted to PDO

--- src/register.php
+++ src/register.php
@@ -7,11 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and password sent from form 
     $hashedPass = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $sql = "INSERT INTO `logintable` (`id`, `Email`, `Password`, `FirstName`, `LastName`) VALUES (NULL,'{$_POST["email"]}','$hashedPass','{$_POST["fname"]}','{$_POST["lname"]}')";
-    $result = mysqli_query($db, $sql);
-
-    if($result != NULL) {
-        header("location: login.php");
-        $_SESSION['created_account'] = "Your account was successfully created! Please log in!";
-    }
+    $statement = $db->prepare($sql);
+    $statement->execute();
+    $statement->closeCursor();
+    $_SESSION['created_account'] = "Your account was successfully created! Please log in!";
+    header("location: login.php");
 }
 ?>
