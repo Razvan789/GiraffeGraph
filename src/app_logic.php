487bb972dd726fcba57672bfbda4a10d1490ddef
Razvan789 <razvanbeldeanu789@gmail.com>
2022-03-23 20:11:06 -0400

    Changed to PDO format

--- src/app_logic.php
+++ src/app_logic.php
@@ -17,14 +17,17 @@ if(isset($_GET['error'])) {
 
 if (isset($_POST['submit_email'])) {
     $errors = [];
-    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $email = $_POST['email'];
     // ensure that the user exists on our system
-    $query = "SELECT Email FROM logintable WHERE Email='$email'";
-    $results = mysqli_query($db, $query);
+    $sql = "SELECT Email FROM logintable WHERE Email='$email'";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
 
     if (empty($email)) {
         array_push($errors, "Your email is required");
-    } else if (mysqli_num_rows($results) <= 0) {
+    } else if (count($results) <= 0) {
         array_push($errors, "Sorry, no user exists on our system with the email: <b>$email</b>");
     }
     // generate a unique random token of length 100
@@ -33,12 +36,14 @@ if (isset($_POST['submit_email'])) {
     if (count($errors) == 0) {
         // store token in the password-reset database table against the user's email
         $sql = "INSERT INTO reset_password(email, token) VALUES ('$email', '$token')";
-        $results = mysqli_query($db, $sql);
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $statement->closeCursor();
 
         // Send email to user with the token in a link they can click on
         $to = $email;
         $subject = "Reset your password on examplesite.com";
-        $msg = "Hi there, visit localhost/GiraffeState/src/newPass.php?token=$token to reset your password on our site";
+        $msg = "Hi there, visit https://www.$host/$site/src/newPass.php?token=$token to reset your password on our site";
         $msg = wordwrap($msg, 70);
         $headers = "From: password-reset@GiraffeState.com";
         mail($to, $subject, $msg, $headers);
@@ -52,17 +57,19 @@ if (isset($_POST['submit_email'])) {
 if(isset($_GET['token'])) {
     $token = $_GET['token'];
     $sql = "SELECT * FROM reset_password WHERE token='$token' LIMIT 1";
-    $results = mysqli_query($db, $sql);
-    $count = mysqli_num_rows($results);
+    $statement = $db->prepare($sql);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
     //Checks to see if the token even exists    
-    if($count < 1) {
+    if(count($results) < 1) {
         header("location: resetPass.php?error=invalidToken");
     }
 }
 
 if (isset($_POST['new_pass'])) {
-    $new_pass = mysqli_real_escape_string($db, $_POST['newPassword']);
-    $new_pass_c = mysqli_real_escape_string($db, $_POST['confirmNewPassword']);
+    $new_pass =  $_POST['newPassword'];
+    $new_pass_c = $_POST['confirmNewPassword'];
 
     // Grab to token that came from the email link
     $token = $_GET['token'];
@@ -71,15 +78,20 @@ if (isset($_POST['new_pass'])) {
     if (count($errors) == 0) {
         // select email address of user from the password_reset table 
         $sql = "SELECT email FROM reset_password WHERE token='$token' LIMIT 1";
-        $results = mysqli_query($db, $sql);
-        $email = mysqli_fetch_assoc($results)['email'];
-
+        $statement = $db->prepare($sql);
+        $results = $statement->fetch();
+        $statement->closeCursor();
+        $email = $results['email'];
         if ($email) {
             $new_pass =  password_hash($new_pass, PASSWORD_DEFAULT);
             $sql = "UPDATE logintable SET password='$new_pass' WHERE email='$email'";
-            $results = mysqli_query($db, $sql);
+            $statement = $db->prepare($sql);
+            $statement->execute();
+            $statement->closeCursor();
             $sql = "DELETE FROM reset_password WHERE token='$token'";
-            mysqli_query($db, $sql);
+            $statement = $db->prepare($sql);
+            $statement->execute();
+            $statement->closeCursor();
             $_SESSION['reset_password'] = "Your password has been reset";
             header('location: login.php');
         }
