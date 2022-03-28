3d13cbef8f7925f58509c73ca88837b463bb58f3
Razvan789 <razvanbeldeanu789@gmail.com>
2022-03-27 23:05:49 -0400

    Now short circuts to check if  and  are set

--- src/messages.php
+++ src/messages.php
@@ -1,12 +1,12 @@
 
-<?php  if (count($messages) > 0) : ?>
+<?php  if (isset($messages) && count($messages) > 0) : ?>
   <div class="msg">
   	<?php foreach ($messages as $message) : ?>
   	  <span style="color:green"><?php echo $message ?></span>
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
-<?php  if (count($errors) > 0) : ?>
+<?php  if (isset($errors) && count($errors) > 0) : ?>
   <div class="msg">
   	<?php foreach ($errors as $error) : ?>
   	  <span style="color:red"><?php echo $error ?></span>
