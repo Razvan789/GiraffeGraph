5c2da4104b5cc3d9e3d400ac47a7cd15b30be25b
Razvan <Razvanbeldeanu789@gmail.com>
2022-04-08 15:44:55 -0400

    Added a note for extra security to be added to the application

--- src/scripts/php_scripts/deleteCanvas.php
+++ src/scripts/php_scripts/deleteCanvas.php
@@ -1,6 +1,7 @@
 <?php
 include("session.php");
 if(isset($_POST['delete'])) {
+    //THERE NEEDS TO BE ANOTHER CHECK HERE THAT THE GID BELONGS TO THE CORRECT USER
     $GID = $_POST['GID'];
     $sql = "DELETE FROM gallery WHERE GID=:gid";
     $statement=$db->prepare($sql);
