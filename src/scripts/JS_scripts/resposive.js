b468d413185573254934bfcf28c637f1b48155e9
Razvan <Razvanbeldeanu789@gmail.com>
2022-05-03 11:39:46 -0400

    Added more responsiveness

--- src/scripts/JS_scripts/resposive.js
+++ src/scripts/JS_scripts/resposive.js
@@ -1,15 +1,19 @@
 const mainBody = document.querySelector(".main-body");
 const hiddenWhenMobile = document.querySelector("#mobile-hidden");
-window.addEventListener('resize',changehomeBody);
+window.addEventListener('resize', changehomeBody);
 window.addEventListener('load', changehomeBody);
 
 function changehomeBody() {
-    if(window.innerWidth <= 990) {
-        mainBody.classList.add("mobile-body");
-        hiddenWhenMobile.classList.add("d-none");
-    } else if(window.innerWidth > 990) {
-        mainBody.classList.remove("mobile-body");
-        hiddenWhenMobile.classList.remove("d-none");
+    if (window.innerWidth <= 990) {
+        if (mainBody)
+            mainBody.classList.add("mobile-body");
+        if (hiddenWhenMobile)
+            hiddenWhenMobile.classList.add("d-none");
+    } else if (window.innerWidth > 990) {
+        if (mainBody)
+            mainBody.classList.remove("mobile-body");
+        if (hiddenWhenMobile)
+            hiddenWhenMobile.classList.remove("d-none");
     }
 }
 
